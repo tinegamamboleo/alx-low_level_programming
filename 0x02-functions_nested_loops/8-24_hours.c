db710da7d3d9337398b91de68472e7398520b636
Mawiti <85223088+Mawiti@users.noreply.github.com>
2021-06-22 21:15:39 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,32 +1,22 @@
 #include "holberton.h"
-
 /**
- * jack_bauer - Prints the minutes of a day
- *
- * Return: no return
+ * jack_bauer - prints time table in this format: HH:MM
+ * Return: nothing
  */
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int h, m;
 
-	for (a = 48; a <= 50; a++)
+	for (h = 0; h < 24; h++)
 	{
-		for (b = 48; b <= 57; b++)
+		for (m = 0; m < 60; m++)
 		{
-			for (c = 48; c <= 53; c++)
-			{
-				for (d = 48; d <= 57; d++)
-				{
-					if (a >= 50 && b >= 52)
-						break;
-					_putchar(a);
-					_putchar(b);
-					_putchar(58);
-					_putchar(c);
-					_putchar(d);
-					_putchar('\n');
-				}
-			}
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
